5d100fc21328efd2480fa1e2b0ed8b3812eb6c73
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-08-22 23:04:42 +0530

    Update Load and Read Dataset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as pn
+import matplotlib.pyplot as plt
 
 housing = pd.read_csv("dataset/boston.csv")
 print(housing.head())
@@ -7,5 +8,12 @@ print("\n")
 print(housing.tail())
 print("\n")
 print(housing.info())
+# crim	zn	indus	chas	nox	rm	age	dis	rad	tax	ptratio	b	lstat	medv
 print("\n")
 print(housing.describe())
+print("\n")
+
+print(housing.hist(bins=50, figsize=(12, 15)))
+# print(plt.show())
+
+# Train Test Spliting
