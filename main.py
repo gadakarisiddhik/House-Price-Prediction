4ff0a0423c657b8606e2184f8471f14a78b15bbb
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-08-23 19:33:50 +0530

    Change House Predication Model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as pn
+import numpy as np
 import matplotlib.pyplot as plt
 
 housing = pd.read_csv("dataset/boston.csv")
@@ -17,3 +17,9 @@ print(housing.hist(bins=50, figsize=(12, 15)))
 # print(plt.show())
 
 # Train Test Spliting
+def split_train_test(data, test_ration):
+    shuffled = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ration)
+    test_indieces = shuffled[:test_set_size]
+    train_indieces = shuffled[test_set_size:]
+    return data.iloc[train_indieces], data.iloc[test_indieces]
