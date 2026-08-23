e21696dee960c000df20a81ade6517fc4c5563f4
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-08-23 20:29:54 +0530

    Added StratifiedShuffleSpli

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 
 housing = pd.read_csv("dataset/boston.csv")
 print(housing.head())
@@ -17,7 +19,12 @@ print(housing.hist(bins=50, figsize=(12, 15)))
 # print(plt.show())
 
 # Train Test Spliting
+
+"""
+--------------------for learnig perpus-------------------
+
 def split_train_test(data, test_ration):
+    np.random.seed(42)
     shuffled = np.random.permutation(len(data))
     test_set_size = int(len(data) * test_ration)
     test_indieces = shuffled[:test_set_size]
@@ -29,3 +36,20 @@ train_set, test_set = split_train_test(
     0.2
 )
 print(f"Rows in train set: {len(train_set)}\nRows in test set: {len(test_set)}")
+
+"""
+
+train_set, test_set = train_test_split(
+    housing,
+    test_size=0.2,
+    random_state=42
+)
+print(f"Rows in train set: {len(train_set)}\nRows in test set: {len(test_set)}")
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing['chas']):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+print(strat_test_set['chas'].value_counts())
+print(strat_train_set['chas'].value_counts())
